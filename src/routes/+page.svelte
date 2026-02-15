91687e748a878aea334aba969a55ae1f8d1c6980
Alex Anderson <anderson.john.alexander@gmail.com>
2026-02-15 11:46:22 -0800

    [WIP] Redirect to dashboard if a user has already created a site
    Redirect to the dashboard instead of the site creation dialog.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,14 @@
 	import { goto } from '$app/navigation'
 	import { check_session } from '$lib/pocketbase/user'
 	import { onMount } from 'svelte'
-
+	/* TODO: Add site counting logic per account see /src/routes/dashboard/sites/+page.svelte? */
 	onMount(async () => {
 		if (await check_session()) {
-			await goto('/admin/site', { replaceState: true })
+			if (sites?.length) {
+				await goto('/admin/dashboard/sites', { replaceState: true })
+			} else {
+				await goto('/admin/site', { replaceState: true })
+			}
 		} else {
 			await goto('/admin/auth', { replaceState: true })
 		}
